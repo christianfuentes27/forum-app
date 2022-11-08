ac2a2b2722d98489b20af80595ada9f5cbd23954
root <cjfuentes27@ieszaidinvergeles.org>
2022-11-08 07:31:07 +0000

    Fix cross modal

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -7,12 +7,12 @@
     <div class="modal-content bg-dark text-white">
       <div class="modal-header">
         <h5 class="modal-title" id="staticBackdropLabel">Confirm delete</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <p>Are you sure you want delete?</p>
+        <p>Are you sure you want to delete?</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
